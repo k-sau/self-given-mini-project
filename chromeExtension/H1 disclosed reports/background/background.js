0dcb829efe5e13629e04aaac55ccfe69722f59f6
Kumar <kumarrockstarsaurabh@gmail.com>
2016-05-10 20:22:51 +0530

    v2.2 Fix for Cloudflare request time out error

--- chromeExtension/H1 disclosed reports/background/background.js
+++ chromeExtension/H1 disclosed reports/background/background.js
@@ -4,41 +4,43 @@ document.addEventListener('DOMContentLoaded', function(){
     window.setInterval(function updateBadge(){
         var newxhttp = new XMLHttpRequest();
         newxhttp.onreadystatechange = function() {
-          if (newxhttp.readyState == 4 && newxhttp.status == 200) {
 
-             newResponse = newxhttp.responseText;
-             newObj = JSON.parse(newResponse);
+        // If request gets failed
+        if(newxhttp.readyState == 4 && newxhttp.status != 200) {
+          updateBadge();
+        }
+        else if(newxhttp.readyState == 4 && newxhttp.status == 200){
 
-            // Reading stored id (popup.js:17)
-            chrome.storage.local.get('firstReportId', function (savedId) {
+        newResponse = newxhttp.responseText;
+        newObj = JSON.parse(newResponse);
 
-              if(typeof savedId.firstReportId == "undefined"){
-                  chrome.browserAction.setBadgeText({
-                  text: "25+"
-                });
-              }
-              else if(newObj.reports[0].id != savedId.firstReportId ){
-                // "count" counts no. of new disclosed reports
-                var count = 0;
-                while(newObj.reports[count].id != savedId.firstReportId){
-                  count++;
-                  if(count > 24){
-                    break;
-                  }
-                }
-                // Shows notification
-                chrome.browserAction.setBadgeText({
-                  text: count.toString()
-                });
-              }
-            });
+       // Reading stored id (popup.js:17)
+       chrome.storage.local.get('firstReportId', function (savedId) {
 
-
-
-       }
-     };
+         if(typeof savedId.firstReportId == "undefined"){
+             chrome.browserAction.setBadgeText({
+             text: "25+"
+           });
+         }
+         else if(newObj.reports[0].id != savedId.firstReportId ){
+           // "count" counts no. of new disclosed reports
+           var count = 0;
+           while(newObj.reports[count].id != savedId.firstReportId){
+             count++;
+             if(count > 24){
+               break;
+             }
+           }
+           // Shows notification
+           chrome.browserAction.setBadgeText({
+             text: count.toString()
+           });
+         }
+       });
+     }
+    };
   newxhttp.open("GET", "https://hackerone.com/hacktivity?page=1&filter=public", true);
   newxhttp.send();
-}, 1800000);
+}, 1000 * (60*10)); //1800000
 
 },false);
