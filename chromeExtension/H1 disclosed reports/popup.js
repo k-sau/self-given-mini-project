d223ec029ea2bd037153a22df49889be69e8502c
Kumar Saurabh <k-sau@users.noreply.github.com>
2016-07-18 20:59:56 +0530

    Fixed public reports fetching url
    Public reports fetching url was changed again...

--- chromeExtension/H1 disclosed reports/popup.js
+++ chromeExtension/H1 disclosed reports/popup.js
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', function(){
       }
     }
   };
-  xhttp.open("GET", "https://hackerone.com/hacktivity/new?page=1&filter=public", true);
+  xhttp.open("GET", "https://hackerone.com/hacktivity/new?page=1&filter=type%3apublic", true);
   xhttp.send();
 
   },false);
